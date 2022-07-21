3b20c40fe6f8cacb13cb0668455b007d73d359bc
uuki <uuki.dev@gmail.com>
2022-07-21 15:05:13 +0900

    Modify sitemap properties

--- sitemap.config.js
+++ sitemap.config.js
@@ -1,6 +1,32 @@
+const { format, parseISO } = require('date-fns')
+
+const EXCLUDES = /^\/(tags\/.*)/
+
+const isIgnorePath = (path) => {
+  return EXCLUDES.test(path)
+}
+
+const getLastmodByPath = (path) => {
+  const data = require(`./.next/server/pages${path.replace(/^\/$/, '/index')}.json`)
+  const props = data.pageProps.page || data.pageProps.post
+  const frontMatter = props ? props.frontMatter : false
+  const lastMod = frontMatter ? parseISO(frontMatter.lastmod) : new Date()
+
+  return format(lastMod, 'yyyy-MM-dd')
+}
+
 module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_META_SITE_URL,
   generateRobotsTxt: true,
   generateIndexSitemap: false,
   outDir: './public',
+  transform: async (config, path) => {
+    if (isIgnorePath(path)) {
+      return null
+    }
+    return {
+      loc: path,
+      lastmod: getLastmodByPath(path),
+    }
+  },
 }
