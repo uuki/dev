15bd1c727db5dad076303b2034a2decfc4873428
uuki <uuki.dev@gmail.com>
2022-10-31 22:15:35 +0900

    chore: addition of units for elapsed time and date

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,27 +1,37 @@
 export const formatSince = (fromDate: Date) => {
   const now: Date = new Date()
   const diff = now.getTime() - fromDate.getTime()
-  const s = Math.round(diff / 1000)
-  const m = Math.round(diff / (1000 * 60))
-  const h = Math.round(diff / (1000 * 60 * 60))
-  const d = Math.round(diff / (1000 * 60 * 60 * 24))
-  const y = Math.round(diff / (1000 * 60 * 60 * 24 * 365))
+  const sec = Math.round(diff / 1000)
+  const min = Math.round(diff / (1000 * 60))
+  const hrs = Math.round(diff / (1000 * 60 * 60))
+  const day = Math.round(diff / (1000 * 60 * 60 * 24))
+  const wek = Math.round(diff / (1000 * 60 * 60 * 24 * 7))
+  const mon = Math.round(diff / (1000 * 60 * 60 * 24 * 30))
+  const yrs = Math.round(diff / (1000 * 60 * 60 * 24 * 365))
 
-  if (s < 60) {
-    return `${s}秒前`
+  if (sec < 60) {
+    return `${sec}秒前`
   }
 
-  if (m < 60) {
-    return `${m}分前`
+  if (min < 60) {
+    return `${min}分前`
   }
 
-  if (h < 24) {
-    return `${h}時間前`
+  if (hrs < 24) {
+    return `${hrs}時間前`
   }
 
-  if (d < 365) {
-    return `${d}日前`
+  if (day < 7) {
+    return `${day}日前`
   }
 
-  return `${y} 年前`
+  if (wek < 4) {
+    return `${wek}週間前`
+  }
+
+  if (mon < 12) {
+    return `${mon}ヶ月前`
+  }
+
+  return `${yrs} 年前`
 }
