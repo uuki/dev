cc4d81b7ea8f6da033faaac522ac19adfc4d9503
uuki <uuki.dev@gmail.com>
2026-05-17 01:17:46 +0900

    feat: deprecate the use of `paginate` separate `page` and `slug` #9
    feat: deprecate the use of `paginate` separate `page` and `slug` #9

---
+++ src/utils/blog.ts
@@ -0,0 +1,11 @@
+import { getCollection } from 'astro:content';
+
+export const POSTS_PER_PAGE = 10;
+
+export const getSortedPosts = async () => {
+  const allPosts = await getCollection('blog', ({ data }) => !data.draft);
+  return allPosts.sort((a, b) => b.data.created_at.getTime() - a.data.created_at.getTime());
+};
+
+export const getBlogPageUrl = (page: number): string =>
+  page === 1 ? '/blog/' : `/blog/page/${page}`;
